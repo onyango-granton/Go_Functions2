37e9afce1c1cf92d0de6f4e50947684d8ad58bba
gonyango <grantononyango@gmail.com>
2024-05-23 16:18:41 +0300

    Main func

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-
-func FindSubString(mainString, subString string) {
+// FindSubString function finds and highlights a substring within a main string.
+func FindSubString(mainString, subString string) string {
 	s1 := []string{}
-
 
 	resString := ""
 
@@ -13,18 +15,19 @@ func FindSubString(mainString, subString string) {
 
 	intArrArr := [][]int{}
 
+	// Convert mainString to a slice of strings
 	for i := 0; i < len(mainString); i++ {
 		s1 = append(s1, string(mainString[i]))
 	}
 
-	// res := ""
-
+	// Iterate through the main string
 	for i := 0; i < len(s1); {
+		// Iterate through the substring
 		for j := 0; j < len(subString); {
+			// Check if characters match
 			if string(subString[j]) == s1[i] {
 				intArr = append(intArr, i)
 
-				// indexes
 				currIndex := intArr[len(intArr)-1]
 				prevIndex := 0
 				if len(intArr) < 2 {
@@ -35,67 +38,55 @@ func FindSubString(mainString, subString string) {
 
 				indexDiff := currIndex - prevIndex
 
-				// fmt.Println(currIndex, indexDiff, string(subString[j]))
-
 				j++
 
+				// Reset if characters are not adjacent
 				if indexDiff > 1 && len(subString) > 1 {
 					j = 0
 					intArr = []int{}
 				}
 
 			}
-			// fmt.Println(j)
 
+			// Store the index array if the substring is found
 			if j == len(subString) {
-				fmt.Println("Here")
 				intArrArr = append(intArrArr, intArr)
 			}
 
 			i++
 
-			// break statement
+			// Break if end of main string is reached
 			if i == len(s1) {
 				break
 			}
 		}
 	}
 
-	// fmt.Println(intArrArr)
-	if len(intArrArr) < 1{
-		fmt.Println("$"+subString+"$ is not present in $"+mainString+"$")
-		return
+	// Return message if substring not found
+	if len(intArrArr) < 1 {
+		msg := "$" + subString + "$ is not present in $" + mainString + "$"
+		return msg
 	}
 
+	// Highlight the substring in the main string
 	for _, ch := range intArrArr {
 		for i := 0; i < len(ch); i++ {
-			// fmt.Println("Here")
 			s1[ch[i]] = "\033[34m" + s1[ch[i]] + "\033[0m"
 		}
 	}
 
-	// for i:=0; i < len(intArr);i++{
-	// 	// fmt.Println("Here")
-	// 	s1[intArr[i]] = "\033[34m"+s1[intArr[i]]+"\033[0m"
-	// }
-
+	// Construct the resulting string
 	for i := 0; i < len(s1); i++ {
 		resString += s1[i]
 	}
 
-	fmt.Println(resString)
+	return resString
 }
 
 func main() {
-	mainString := "A God morning is A Good God glory Morning Good"
-	subString := "fire"
-	FindSubString(mainString, subString)
+	// mainString := "A God morning is A Good God glory Morning Good"
+	// subString := "oo"
+	mainString := os.Args[1]
+	subString := os.Args[2]
+	fmt.Println(FindSubString(mainString, subString))
 }
-
-// if indexDiff == 1{
-// 	// 	// fmt.Println(currIndex, prevIndex, string(subString[j]))
-// 	// }
-// 	// if indexDiff > 1{
-// 	// 	intArr = []int{}
-// 	// 	j = 0
-// 	// }
